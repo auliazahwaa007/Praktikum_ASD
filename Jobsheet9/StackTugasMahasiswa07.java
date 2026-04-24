1e0af7a333b17a9e75414a1ef41a180149d319b4
OLIAA <auliazahwaa007@gmail.com>
2026-04-24 13:41:31 +0700

    modifikasi 1

--- Jobsheet9/StackTugasMahasiswa07.java
+++ Jobsheet9/StackTugasMahasiswa07.java
@@ -51,7 +51,7 @@ public class StackTugasMahasiswa07 {
         }
     }
     public void print(){
-        for (int i = 0; i <= top; i++){
+        for (int i = top; i >= 0; i--){
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.println("");
