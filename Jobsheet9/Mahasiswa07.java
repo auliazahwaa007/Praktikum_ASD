74891ca38aaf0ee551548d10b4716c8a2b5a7970
OLIAA <auliazahwaa007@gmail.com>
2026-04-24 13:41:00 +0700

    program awal

---
+++ Jobsheet9/Mahasiswa07.java
@@ -0,0 +1,16 @@
+package Jobsheet9;
+
+public class Mahasiswa07 {
+    String nim, nama, kelas;
+    int nilai;
+
+    Mahasiswa07(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+    void tugasDinilai(int nilai){
+        this.nilai = nilai;
+    }
+}
